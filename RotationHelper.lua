ce5aeb16a501efb6006d243ad3e4a95cd9bd5729
Ori <buhbutoriasher@gmail.com>
2025-05-31 22:21:58 +0300

    disable prints

--- RotationHelper.lua
+++ RotationHelper.lua
@@ -68,7 +68,7 @@ function RotationHelper:GetYPRFromLUF(left, up, forward)
 	-- Special case, forward is (0,1,0) or (0,-1,0)
 	if forward.x == 0 and forward.z == 0 then
 		local yaw = 0
-		local pitch = forward.y * pi/2
+		local pitch = forward.y * pi / 2
 		local roll = asin(-up.x)
 
 		return yaw, pitch, roll
@@ -79,14 +79,14 @@ function RotationHelper:GetYPRFromLUF(left, up, forward)
 	local yaw = atan(forward.x, forward.z)
 	local roll = 0
 
-	local y = Vec3(0,1,0)
+	local y = Vec3(0, 1, 0)
 
 	-- r0 is the right vector before pitch rotation
-	local r0 = Vec3(0,0,0)
+	local r0 = Vec3(0, 0, 0)
 	local r1 = y:Cross(forward)
 
 	-- Normalizing r0
-	local mod_r1 = sqrt(r1.x^2 + r1.y^2 + r1.z^2)
+	local mod_r1 = sqrt(r1.x ^ 2 + r1.y ^ 2 + r1.z ^ 2)
 
 	r0.x = r1.x / mod_r1
 	r0.y = r1.y / mod_r1
@@ -98,20 +98,20 @@ function RotationHelper:GetYPRFromLUF(left, up, forward)
 	local cosPitch = u0:Dot(up)
 
 	if r0.x > r0.y and r0.x > r0.z and r0.x ~= 0 then
-		roll = asin( (u0.x * cosPitch - up.x) / r0.x)
+		roll = asin((u0.x * cosPitch - up.x) / r0.x)
 	elseif r0.y > r0.x and r0.y > r0.z and r0.y ~= 0 then
-		roll = asin( (u0.y * cosPitch - up.y) / r0.y)
+		roll = asin((u0.y * cosPitch - up.y) / r0.y)
 	elseif r0.z > r0.x and r0.z > r0.y and r0.z ~= 0 then
-		roll = asin( (u0.z * cosPitch - up.z) / r0.z)
+		roll = asin((u0.z * cosPitch - up.z) / r0.z)
 	else
 		if r0.x ~= 0 then
-			roll = asin( (u0.x * cosPitch - up.x) / r0.x)
+			roll = asin((u0.x * cosPitch - up.x) / r0.x)
 		elseif r0.y ~= 0 then
-			roll = asin( (u0.y * cosPitch - up.y) / r0.y)
+			roll = asin((u0.y * cosPitch - up.y) / r0.y)
 		elseif r0.z ~= 0 then
-			roll = asin( (u0.z * cosPitch - up.z) / r0.z)
+			roll = asin((u0.z * cosPitch - up.z) / r0.z)
 		else
-			print("[RotationHelper] All denominators are 0, something went wrong")
+			--print("[RotationHelper] All denominators are 0, something went wrong")
 		end
 	end
 
@@ -169,7 +169,7 @@ end
 --------------Linear Transform variants-------
 function RotationHelper:GetYPRFromLT(linearTransform)
 	if linearTransform.typeInfo.name == nil or linearTransform.typeInfo.name ~= "LinearTransform" then
-		print("[RotationHelper] Wrong argument for GetYPRFromLT, expected LinearTransform")
+		--print("[RotationHelper] Wrong argument for GetYPRFromLT, expected LinearTransform")
 
 		return
 	end
@@ -186,9 +186,8 @@ end
 function RotationHelper:GetLTFromYPR(yaw, pitch, roll)
 	local left, up, forward = self:GetLUFFromYPR(yaw, pitch, roll)
 
-	return LinearTransform(left, up, forward, Vec3(0,0,0))
+	return LinearTransform(left, up, forward, Vec3(0, 0, 0))
 end
-
 
 -------------Util--------------------
 function RotationHelper:AdjustRange(vIn, offset, rightLimit)
@@ -198,7 +197,7 @@ function RotationHelper:AdjustRange(vIn, offset, rightLimit)
 		v = v - rightLimit
 	end
 	if (v < 0) then
-	v = v + rightLimit;
+		v = v + rightLimit;
 	end
 	return v
 end
